867aa9f6934f2e4b932d03600f8e232337656225
Calvin Mann <calvin_m@hotmail.com>
2018-08-21 11:39:36 -0700

    Fixed the connection string

--- Alfred.WebApi/DependencyInjectionModule.cs
+++ Alfred.WebApi/DependencyInjectionModule.cs
@@ -1,5 +1,8 @@
 ﻿using Alfred.Application.Repositories;
+using Alfred.Application.UseCases.RegisterSubscriber;
+using Alfred.Infrastructure.Repositories.EntityFramework;
 using Autofac;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,23 @@ namespace Alfred.WebApi
 {
     public class DependencyInjectionModule : Module
     {
+        public string ConnectionString { get; set; }
+
         protected override void Load(ContainerBuilder builder)
         {
+            ConnectionString = @"Server=CALVINMANN505B\\SQLEXPRESS;Database=ArbitrageSearch;Trusted_Connection=True;MultipleActiveResultSets=true";
             builder.RegisterType<EFInfra.SubscriberRepository>().As<ISubscriberWriteOnlyRepository>();
+            builder.RegisterType<RegisterSubscriberAppService>().As<IRegisterSubscriberAppService>();
             //builder.RegisterType<>().As<IAccountWriteOnlyRepository>
+
+            var optionsBuilder = new DbContextOptionsBuilder<DbContext>();
+            optionsBuilder.UseSqlServer(ConnectionString);
+            optionsBuilder.EnableSensitiveDataLogging(true);
+
+            builder.RegisterType<AlfredDbContext>()
+              .WithParameter(new TypedParameter(typeof(DbContextOptions), optionsBuilder.Options))
+              .InstancePerLifetimeScope();
+
         }
     }
 }
